feabaa415598b03979b2fe6dea9f18037ae12afe
zuowenjian <zuowenjian@ayibang.com>
2015-08-02 11:31:22 +0800

    [0.1.3.124] xxx

--- src/main_impl.py
+++ src/main_impl.py
@@ -40,6 +40,8 @@ def main():
     if setting.debug :
         rargs  = impl.rg_args.run_args.load()
         rargs.parse_update(parser)
+        print(parser.argv)
+        return ;
         impl.rg_run.run_rigger(rargs,parser.argv)
     else:
         try :
