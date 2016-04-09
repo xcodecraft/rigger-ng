8f6c998844cd9b1f9d622e1a659a50457559acab
zuowenjian <zuowenjian@ayibang.com>
2016-04-09 20:26:51 +0800

    [0.7.0.234]

--- src/rg_envs/centos_ayb.py
+++ src/rg_envs/centos_ayb.py
@@ -5,7 +5,7 @@ def rgenv_enable() :
     rgenv['PHP_FPM']    = "/usr/local/php-5.6/sbin/php-fpm"
     rgenv['PHP_INI']    = "/usr/local/php-5.6/lib/php.ini"
     rgenv['VARNISHD']   = "/usr/local/varnish-4.1.2/sbin/varnishd"
-    rgenv['VARNISHADM'] = "/usr/local/lvarnish-4.1.2/bin/varnishadm"
+    rgenv['VARNISHADM'] = "/usr/local/varnish-4.1.2/bin/varnishadm"
     rgenv['RG_DEVPATH'] = "/data/${USER}/devspace/rigger-ng"
     rgenv['ZDAEMON']    = "/usr/local/python/bin/zdaemon"
     rgenv['BEANSTALKD'] = "/usr/local/beanstalkd/bin/beanstalkd"
