adfd820f7b1c470f26e8cb4b2adc02f8910a718e
zuowenjian <zuowenjian@ayibang.com>
2016-04-09 21:11:36 +0800

    add plat_tpl

---
+++ src/rg_envs/plat_tpl.py
@@ -0,0 +1,13 @@
+from setting import rgenv
+
+def rgenv_enable() :
+    rgenv['PHP_BIN']    = ""
+    rgenv['PHP_FPM']    = ""
+    rgenv['PHP_INI']    = ""
+    rgenv['VARNISHD']   = ""
+    rgenv['VARNISHADM'] = ""
+    rgenv['RG_DEVPATH'] = ""
+    rgenv['ZDAEMON']    = ""
+    rgenv['BEANSTALKD'] = ""
+    rgenv['NGINX_CONF'] = ""
+
