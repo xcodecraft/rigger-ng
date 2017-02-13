c3d82a0e7dcb4f58f4ba8572faeb1a2729bc7f50
zuowenjian <zuowenjian@deepclue.cn>
2017-02-13 21:14:47 +0800

    [0.12.2.305]

--- simple_setup.sh
+++ simple_setup.sh
@@ -1,6 +1,7 @@
-DST=/home/x/tools/rigger-ng
-PKG=/home/x/tools/pkgs/rigger-ng-simple
+DST=/data/x/tools/rigger-ng
+PKG=/data/x/tools/pkgs/rigger-ng-simple
 rm -rf $DST
+mkdir -p /data/x/tools/pkgs
 cp -r ./ $PKG
 ln -s $PKG/src  $DST
 cd $DST ;
