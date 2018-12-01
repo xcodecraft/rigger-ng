bc482d5556a18b4a1fa35eb37e6ff2356fc63167
左文建 <zuowenjian@360.net>
2018-12-01 18:39:56 +0800

    #38

--- myrg.bashrc
+++
@@ -1,4 +0,0 @@
-RG_ROOT=$HOME/devspace/rigger-ng/src
-alias rg='$RG_ROOT/rg'
-alias srg='sudo $RG_ROOT/rg'
-VER=`cat $RG_ROOT/version.txt`
--- setup/mac.sh
+++ setup/mac.sh
@@ -1,5 +1,14 @@
 DST=${HOME}/rigger-ng
 PKG=${HOME}/devspace/rigger-ng/src
+
+unlink $DST
 ln -s $PKG $DST
 cd $DST ;
 ./setup.sh mac.py
+cd /usr/local/bin
+sudo unlink rg
+sudo ln -s ${DST}/rg rg
+
+
+echo "Welcome use Rigger-NG! "
+echo "eg: rg help"
